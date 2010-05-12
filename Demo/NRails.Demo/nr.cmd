cb0181fa446c753821e4f9aa025686745b0806d9
Alex Zimin <ziminav@gmail.com>
2010-05-12 23:52:35 +0800

    запуск миграций теперь выдает внятное сообщение об ошибке, если рельсы не были собраны

--- Demo/NRails.Demo/nr.cmd
+++ Demo/NRails.Demo/nr.cmd
@@ -1,1 +1,12 @@
+@echo off
+
+if not exist lib\nrails\NRails.Console.exe goto MAKE_BOOT_WARN
+
 lib\nrails\NRails.Console.exe %*
+goto EXIT
+
+:MAKE_BOOT_WARN
+echo Error: lib\nrails\NRails.Console.exe not found, run make_boot.cmd from nrails solution folder first
+
+:EXIT
+
